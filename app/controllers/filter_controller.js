196bb562a7fea141141d23a61098db486ca33f5a
MC <chokamoses2@gmail.com>
2025-12-26 16:44:01 +0300

    enabled auto-submit when a user search for property

---
+++ app/controllers/filter_controller.js
@@ -0,0 +1,9 @@
+// auto submit forms on changes
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = [ "form" ]
+    submit() {
+        this.formTarget.requestSubmit()
+    }
+}
